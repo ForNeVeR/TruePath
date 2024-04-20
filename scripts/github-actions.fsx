4464d0f427f2a1964d804cc13acaeb494408bd61
Friedrich von Never <friedrich@fornever.me>
2024-04-20 20:38:46 +0200

    CI: enable encoding check

--- scripts/github-actions.fsx
+++ scripts/github-actions.fsx
@@ -4,6 +4,7 @@
 
 #r "nuget: Generaptor.Library, 1.2.0"
 
+open System
 open Generaptor
 open Generaptor.GitHubActions
 open type Generaptor.GitHubActions.Commands
@@ -11,20 +12,39 @@ open type Generaptor.Library.Actions
 open type Generaptor.Library.Patterns
 
 let mainBranch = "main"
+let ubuntu = "ubuntu-22.04"
+let images = [
+    "macos-12"
+    ubuntu
+    "windows-2022"
+]
+
 
 let workflows = [
-    workflow "main" [
-        name "Main"
+    let mainTriggers = [
         onPushTo mainBranch
         onPullRequestTo mainBranch
+        onSchedule(day = DayOfWeek.Saturday)
         onWorkflowDispatch
+    ]
+
+    workflow "main" [
+        name "Main"
+        yield! mainTriggers
 
         job "licenses" [
-            runsOn "ubuntu-22.04"
+            runsOn ubuntu
             checkout
 
             step(name = "REUSE license check", uses = "fsfe/reuse-action@v3")
         ]
+        job "encoding" [
+            runsOn ubuntu
+            checkout
+
+            step(name = "Verify encoding", shell = "pwsh", run = "scripts/Test-Encoding.ps1")
+        ]
+
     ]
 ]
 
