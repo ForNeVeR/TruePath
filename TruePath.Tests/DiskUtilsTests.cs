03dc11c2dbaf815fcad275c9a9a621f11b9af753
Kataane <kataanee@gmail.com>
2024-07-18 10:50:05 +0700

    Fix GetPosixRealPath

--- TruePath.Tests/DiskUtilsTests.cs
+++ TruePath.Tests/DiskUtilsTests.cs
@@ -1,9 +1,31 @@
+using RuntimeInformation = System.Runtime.InteropServices.RuntimeInformation;
+
 namespace TruePath.Tests;
 
 public class DiskUtilsTests
 {
     [Fact]
-    public void DiskUtils_OnWindows_PassPath_ReturnCanonicalPath()
+    public void DiskUtils_OnWindows_PassBackPath_ReturnCanonicalPath()
+    {
+        // Arrange
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return;
+
+        var currentDirectory = Environment.CurrentDirectory;
+        var directories = currentDirectory.Split(@"\").ToList();
+        var back = Random.Shared.Next(directories.Count);
+        var expected = Back(directories, back, @"\");
+
+        var nonCanonicalPath = new string(currentDirectory) + string.Concat(Enumerable.Repeat(@"\..", back));
+
+        // Act
+        var actual = DiskUtils.GetWindowsRealPathByPath(nonCanonicalPath);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void DiskUtils_OnWindows_PassNonCanonicalPath_ReturnCanonicalPath()
     {
         // Arrange
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return;
@@ -19,19 +41,36 @@ public class DiskUtilsTests
     }
 
     [Fact]
-    public void DiskUtils_NonWindows_PassPath_ReturnCanonicalPath()
+    public void DiskUtils_Posix_PassBackPath_ReturnCanonicalPath()
     {
         // Arrange
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return;
 
-        var expected = Environment.CurrentDirectory;
-        var nonCanonicalPath = new string(MakeNonCanonicalPath(expected).ToArray());
+        var currentDirectory = Environment.CurrentDirectory;
+        var directories = currentDirectory.Split("/").ToList();
+        var back = Random.Shared.Next(directories.Count);
+        var expected = Back(directories, back, "/");
+
+        var nonCanonicalPath = new string(currentDirectory) + string.Concat(Enumerable.Repeat("/..", back));
 
         // Act
         var actual = DiskUtils.GetPosixRealPath(nonCanonicalPath);
 
         // Assert
         Assert.Equal(expected, actual);
+    }
+
+    private static string Back(List<string> folders, int stepsBack, string delimiter)
+    {
+        int finalIndex = folders.Count - stepsBack - 1;
+        List<string> finalFolders = folders[..(finalIndex + 1)];
+
+        if (finalFolders.Count == 1)
+        {
+            return delimiter;
+        }
+
+        return string.Join(delimiter, finalFolders);
     }
 
     private static IEnumerable<char> MakeNonCanonicalPath(string path)
--- TruePath/DiskUtils.cs
+++ TruePath/DiskUtils.cs
@@ -1,28 +1,26 @@
 using System.Buffers;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Runtime.InteropServices.Marshalling;
+using Microsoft.Win32.SafeHandles;
 
 // ReSharper disable InconsistentNaming
 
 namespace TruePath;
 
-public static unsafe partial class Syscall
+public static partial class Syscall
 {
-    private const string LIBC_6 = "libc";
-
-    [LibraryImport(LIBC_6, SetLastError = true)]
-    public static partial int readlink(
-        [MarshalAs(UnmanagedType.LPStr)] string path,
-        byte* buffer, int bufferSize);
+    [LibraryImport("libc",
+        EntryPoint = "realpath",
+        SetLastError = true,
+        StringMarshalling = StringMarshalling.Custom,
+        StringMarshallingCustomType = typeof(AnsiStringMarshaller))]
+    internal static partial SafeFileHandle realpath(string path, IntPtr buffer);
 }
 
-public static class DiskUtils
+public static partial class DiskUtils
 {
     // from https://github.com/dotnet/corefx/blob/9c06da6a34fcefa6fb37776ac57b80730e37387c/src/Common/src/System/IO/PathInternal.Windows.cs#L52
     public const short WindowsMaxPath = short.MaxValue;
-
-    public const int LinuxMaxPath = 4096;
-
     public static string GetAutoRealPath(string path)
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
@@ -33,52 +31,36 @@ public static class DiskUtils
         return GetPosixRealPath(path);
     }
 
-    public static unsafe string GetPosixRealPath(string path)
+    public static string GetPosixRealPath(string path)
     {
-        var byteArray = ArrayPool<byte>.Shared.Rent(LinuxMaxPath);
+        using var ptr = Syscall.realpath(path, IntPtr.Zero);
 
-        fixed (byte* buffer = byteArray)
-        {
-            var result = Syscall.readlink(path, buffer, LinuxMaxPath);
-            if (result == -1)
-            {
-                ArrayPool<byte>.Shared.Return(byteArray);
-
-                // not a symbolic link
-                return path;
-            }
-
-            var realPath = Encoding.UTF8.GetString(byteArray, 0, result);
-
-            ArrayPool<byte>.Shared.Return(byteArray);
-
-            return realPath;
-        }
-    }
-
-    public static string GetWindowsRealPathByPath(string path)
-    {
-        var handle = CreateFile(path,
-            FILE_READ_EA,
-            FileShare.Read,
-            IntPtr.Zero,
-            FileMode.Open,
-            FILE_FLAG_BACKUP_SEMANTICS,
-            IntPtr.Zero);
-
-        if (handle == INVALID_HANDLE_VALUE)
+        if (ptr.DangerousGetHandle() == IntPtr.Zero)
         {
             return path;
         }
 
-        try
+        var result = Marshal.PtrToStringAnsi(ptr.DangerousGetHandle());
+
+        return result ?? path;
+    }
+
+    public static string GetWindowsRealPathByPath(string path)
+    {
+        using var handle = CreateFile(path,
+            FileAccess.ReadEa,
+            FileShare.Read,
+            IntPtr.Zero,
+            FileMode.Open,
+            FileAttributes.BackupSemantics,
+            IntPtr.Zero);
+
+        if (handle.IsInvalid)
         {
-            return GetWindowsRealPathByHandle(handle) ?? path;
+            return path;
         }
-        finally
-        {
-            CloseHandle(handle);
-        }
+
+        return GetWindowsRealPathByHandle(handle.DangerousGetHandle()) ?? path;
     }
 
     private static unsafe string? GetWindowsRealPathByHandle(IntPtr handle)
@@ -117,7 +99,7 @@ public static class DiskUtils
 
             fixed (char* buffer = charArray)
             {
-                var result = GetFinalPathNameByHandle(handle, buffer, bufferSize, 0);
+                var result = GetFinalPathNameByHandle(handle, buffer, bufferSize);
                 if (result == 0)
                 {
                     outputPath = null;
@@ -140,28 +122,188 @@ public static class DiskUtils
         }
     }
 
-    private const uint FILE_READ_EA = 0x0008;
-    private const uint FILE_FLAG_BACKUP_SEMANTICS = 0x2000000;
-    private static readonly IntPtr INVALID_HANDLE_VALUE = new(-1);
+    [Flags]
+    internal enum FileAttributes : uint
+    {
+        /// <summary>FILE_ATTRIBUTE_ARCHIVE</summary>
+        Archive = 0x20,
 
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    private static extern IntPtr CreateFile(
+        /// <summary>FILE_ATTRIBUTE_COMPRESSED</summary>
+        Compressed = 0x800,
+
+        /// <summary>FILE_ATTRIBUTE_DEVICE</summary>
+        Device = 0x40,
+
+        /// <summary>FILE_ATTRIBUTE_DIRECTORY</summary>
+        Directory = 0x10,
+
+        /// <summary>FILE_ATTRIBUTE_ENCRYPTED</summary>
+        Encrypted = 0x4000,
+
+        /// <summary>FILE_ATTRIBUTE_HIDDEN</summary>
+        Hidden = 0x02,
+
+        /// <summary>FILE_ATTRIBUTE_INTEGRITY_STREAM</summary>
+        IntegrityStream = 0x8000,
+
+        /// <summary>FILE_ATTRIBUTE_NORMAL</summary>
+        Normal = 0x80,
+
+        /// <summary>FILE_ATTRIBUTE_NOT_CONTENT_INDEXED</summary>
+        NotContentIndexed = 0x2000,
+
+        /// <summary>FILE_ATTRIBUTE_NO_SCRUB_DATA</summary>
+        NoScrubData = 0x20000,
+
+        /// <summary>FILE_ATTRIBUTE_OFFLINE</summary>
+        Offline = 0x1000,
+
+        /// <summary>FILE_ATTRIBUTE_READONLY</summary>
+        Readonly = 0x01,
+
+        /// <summary>FILE_ATTRIBUTE_REPARSE_POINT</summary>
+        ReparsePoint = 0x400,
+
+        /// <summary>FILE_ATTRIBUTE_SPARSE_FILE</summary>
+        SparseFile = 0x200,
+
+        /// <summary>FILE_ATTRIBUTE_SYSTEM</summary>
+        System = 0x04,
+
+        /// <summary>FILE_ATTRIBUTE_TEMPORARY</summary>
+        Temporary = 0x100,
+
+        /// <summary>FILE_ATTRIBUTE_VIRTUAL</summary>
+        Virtual = 0x10000,
+
+        /// <summary>FILE_FLAG_BACKUP_SEMANTICS</summary>
+        BackupSemantics = 0x02000000,
+
+        /// <summary>FILE_FLAG_DELETE_ON_CLOSE</summary>
+        DeleteOnClose = 0x04000000,
+
+        /// <summary>FILE_FLAG_NO_BUFFERING</summary>
+        NoBuffering = 0x20000000,
+
+        /// <summary>FILE_FLAG_OPEN_NO_RECALL</summary>
+        OpenNoRecall = 0x00100000,
+
+        /// <summary>FILE_FLAG_OPEN_REPARSE_POINT</summary>
+        OpenReparsePoint = 0x00200000,
+
+        /// <summary>FILE_FLAG_OVERLAPPED</summary>
+        Overlapped = 0x40000000,
+
+        /// <summary>FILE_FLAG_POSIX_SEMANTICS</summary>
+        PosixSemantics = 0x0100000,
+
+        /// <summary>FILE_FLAG_RANDOM_ACCESS</summary>
+        RandomAccess = 0x10000000,
+
+        /// <summary>FILE_FLAG_SESSION_AWARE</summary>
+        SessionAware = 0x00800000,
+
+        /// <summary>FILE_FLAG_SEQUENTIAL_SCAN</summary>
+        SequentialScan = 0x08000000,
+
+        /// <summary>FILE_FLAG_WRITE_THROUGH</summary>
+        WriteThrough = 0x80000000
+    }
+
+    [Flags]
+    internal enum FileAccess : uint
+    {
+        /// <summary>FILE_READ_DATA</summary>
+        ReadData = 0x0001,
+
+        /// <summary>FILE_LIST_DIRECTORY</summary>
+        ListDirectory = ReadData,
+
+        /// <summary>FILE_WRITE_DATA</summary>
+        WriteData = 0x0002,
+
+        /// <summary>FILE_ADD_FILE</summary>
+        AddFile = WriteData,
+
+        /// <summary>FILE_APPEND_DATA</summary>
+        AppendData = 0x0004,
+
+        /// <summary>FILE_ADD_SUBDIRECTORY</summary>
+        AddSubdirectory = AppendData,
+
+        /// <summary>FILE_CREATE_PIPE_INSTANCE</summary>
+        CreatePipeInstance = AppendData,
+
+        /// <summary>FILE_READ_EA</summary>
+        ReadEa = 0x0008,
+
+        /// <summary>FILE_WRITE_EA</summary>
+        WriteEa = 0x0010,
+
+        /// <summary>FILE_EXECUTE</summary>
+        Execute = 0x0020,
+
+        /// <summary>FILE_TRAVERSE</summary>
+        Traverse = Execute,
+
+        /// <summary>FILE_DELETE_CHILD</summary>
+        DeleteChild = 0x0040,
+
+        /// <summary>FILE_READ_ATTRIBUTES</summary>
+        ReadAttributes = 0x0080,
+
+        /// <summary>FILE_WRITE_ATTRIBUTES</summary>
+        WriteAttributes = 0x0100,
+
+        /// <summary>GENERIC_READ</summary>
+        GenericRead = 0x80000000,
+
+        /// <summary>GENERIC_WRITE</summary>
+        GenericWrite = 0x40000000,
+
+        /// <summary>GENERIC_EXECUTE</summary>
+        GenericExecute = 0x20000000,
+
+        /// <summary>GENERIC_ALL</summary>
+        GenericAll = 0x10000000
+    }
+
+    [Flags]
+    internal enum FileShare : uint
+    {
+        /// <summary>FILE_SHARE_NONE</summary>
+        None = 0x00,
+
+        /// <summary>FILE_SHARE_READ</summary>
+        Read = 0x01,
+
+        /// <summary>FILE_SHARE_WRITE</summary>
+        Write = 0x02,
+
+        /// <summary>FILE_SHARE_DELETE</summary>
+        Delete = 0x03
+    }
+
+    [LibraryImport("kernel32.dll",
+        EntryPoint = "CreateFileW",
+        SetLastError = true,
+        StringMarshalling = StringMarshalling.Utf16)]
+    internal static partial SafeFileHandle CreateFile(
         [MarshalAs(UnmanagedType.LPTStr)] string filename,
-        [MarshalAs(UnmanagedType.U4)] uint access,
-        [MarshalAs(UnmanagedType.U4)] FileShare share,
-        IntPtr securityAttributes, // optional SECURITY_ATTRIBUTES struct or IntPtr.Zero
-        [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
-        [MarshalAs(UnmanagedType.U4)] uint flagsAndAttributes,
-        IntPtr templateFile);
+        FileAccess access,
+        FileShare share,
+        nint securityAttributes, // optional SECURITY_ATTRIBUTES struct or IntPtr.Zero
+        FileMode creationDisposition,
+        FileAttributes flagsAndAttributes,
+        nint templateFile);
 
-    [DllImport("kernel32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool CloseHandle(IntPtr hObject);
-
-    [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-    private static extern unsafe uint GetFinalPathNameByHandle(
+    [LibraryImport("kernel32.dll",
+        EntryPoint = "GetFinalPathNameByHandleW",
+        SetLastError = true,
+        StringMarshalling = StringMarshalling.Utf16)]
+    private static unsafe partial uint GetFinalPathNameByHandle(
         IntPtr hFile,
         char* buffer,
         uint bufferLength,
-        uint dwFlags);
+        uint dwFlags = 0);
 }
