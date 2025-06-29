d02751474f782fa3a514c697656853c632fc326d
Friedrich von Never <friedrich@fornever.me>
2025-06-29 22:39:46 +0200

    NormalizationTests: some code cleanup

--- TruePath.Tests/NormalizationTests.cs
+++ TruePath.Tests/NormalizationTests.cs
@@ -1,24 +1,18 @@
-// SPDX-FileCopyrightText: 2024 TruePath contributors <https://github.com/ForNeVeR/TruePath>
+// SPDX-FileCopyrightText: 2024-2025 TruePath contributors <https://github.com/ForNeVeR/TruePath>
 //
 // SPDX-License-Identifier: MIT
-
-namespace TruePath.Tests;
 
 using FsCheck;
 using FsCheck.Fluent;
 using FsCheck.Xunit;
-using System.Linq;
+using JetBrains.Annotations;
 using Xunit.Abstractions;
 
-public class NormalizatonTests
-{
-    private readonly ITestOutputHelper _TestOutputHelper;
-    public NormalizatonTests(ITestOutputHelper testOutputHelper)
-    {
-        _TestOutputHelper = testOutputHelper;
-    }
+namespace TruePath.Tests;
 
-    [Property(Arbitrary = new[] { typeof(AnyOsPath) })]
+public class NormalizationTests(ITestOutputHelper testOutputHelper)
+{
+    [Property(Arbitrary = [typeof(AnyOsPath)])]
     public void NormalizedPathDoesNotEndWithDirSeparator(List<string> pathParts)
     {
         var sourcePath = string.Join("", pathParts);
@@ -26,14 +20,14 @@ public class NormalizatonTests
         // Act
         var normalizedPath = PathStrings.Normalize(sourcePath);
 
-        _TestOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
+        testOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
         Assert.True(normalizedPath == ""
             || normalizedPath == Path.DirectorySeparatorChar.ToString()
-            || (normalizedPath.Length == 3 && normalizedPath[1] == ':')
+            || normalizedPath is [_, ':', _]
             || normalizedPath[^1] != Path.DirectorySeparatorChar);
     }
 
-    [Property(Arbitrary = new[] { typeof(AnyOsPath) })]
+    [Property(Arbitrary = [typeof(AnyOsPath)])]
     public void NormalizedPathDoesNotContainAltDirSeparator(List<string> pathParts)
     {
         var sourcePath = string.Join("", pathParts);
@@ -41,11 +35,11 @@ public class NormalizatonTests
         // Act
         var normalizedPath = PathStrings.Normalize(sourcePath);
 
-        _TestOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
+        testOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
         Assert.True(!normalizedPath.Contains(Path.AltDirectorySeparatorChar));
     }
 
-    [Property(Arbitrary = new[] { typeof(AnyOsPath) })]
+    [Property(Arbitrary = [typeof(AnyOsPath)])]
     public void NormalizedPathDoesNotContainTwoDirSeparator(List<string> pathParts)
     {
         var sourcePath = string.Join("", pathParts);
@@ -53,19 +47,19 @@ public class NormalizatonTests
         // Act
         var normalizedPath = PathStrings.Normalize(sourcePath);
 
-        _TestOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
+        testOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
         Assert.True(!normalizedPath.Contains(Path.DirectorySeparatorChar.ToString() + Path.DirectorySeparatorChar));
     }
 
-    [Property(Arbitrary = new[] { typeof(AnyOsPath) })]
-    public void DepthPreserverd(List<string> pathParts)
+    [Property(Arbitrary = [typeof(AnyOsPath)])]
+    public void DepthPreserved(List<string> pathParts)
     {
         var sourcePath = string.Join("", pathParts);
 
         // Act
         var normalizedPath = PathStrings.Normalize(sourcePath);
 
-        _TestOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
+        testOutputHelper.WriteLine($"{sourcePath} => {normalizedPath}");
         var collapsedBlock = CollapseSameBlocks(pathParts);
         var expectedDepth = CountDepth(collapsedBlock);
         var actualDepth = CountDepth([.. normalizedPath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries)]);
@@ -103,7 +97,7 @@ public class NormalizatonTests
         }
     }
 
-    private static List<string> CollapseSameBlocks(IEnumerable<string> pathParts)
+    private static List<string> CollapseSameBlocks(IReadOnlyCollection<string> pathParts)
     {
         var result = new List<string>();
         var lastSeparator = false;
@@ -111,10 +105,7 @@ public class NormalizatonTests
         string? homeDrive = null;
         foreach (var item in pathParts)
         {
-            if (homeDrive is null)
-            {
-                homeDrive = item;
-            }
+            homeDrive ??= item;
 
             if (item.Length > 1 && item[1] == ':') continue;
 
@@ -161,12 +152,16 @@ internal static class PathGenerators
         var baseDir = Gen.Constant("..");
         var currentDir = Gen.Constant(".");
         var threeDots = Gen.Constant("...");
-        var dots = Gen.OneOf([baseDir, currentDir, threeDots]);
-        var textPath = Gen.NonEmptyListOf(Gen.Choose('a', 'z').Or(Gen.Choose('A', 'Z')).Or(Gen.Choose('0', '9'))).Select(l => string.Join("", l.Select(c => (char)c)));
+        var dots = Gen.OneOf(baseDir, currentDir, threeDots);
+        var textPath = Gen.Choose('a', 'z')
+            .Or(Gen.Choose('A', 'Z'))
+            .Or(Gen.Choose('0', '9'))
+            .NonEmptyListOf()
+            .Select(l => string.Join("", l.Select(c => (char)c)));
 
         var directorySeparatorChar = Gen.Constant(Path.DirectorySeparatorChar).Select(c => c.ToString());
         var altDirectorySeparatorChar = Gen.Constant(Path.AltDirectorySeparatorChar).Select(c => c.ToString());
-        return Gen.NonEmptyListOf(Gen.OneOf([dots, directorySeparatorChar, altDirectorySeparatorChar, textPath]))
+        return Gen.OneOf(dots, directorySeparatorChar, altDirectorySeparatorChar, textPath).NonEmptyListOf()
             .Where(l =>
             {
                 for (int i = 0; i < l.Count - 1; i++)
@@ -185,12 +180,12 @@ internal static class PathGenerators
         var directorySeparatorChar = Gen.Constant(Path.DirectorySeparatorChar).Select(c => c.ToString());
         var altDirectorySeparatorChar = Gen.Constant(Path.AltDirectorySeparatorChar).Select(c => c.ToString());
         var separator = Gen.OneOf([directorySeparatorChar, altDirectorySeparatorChar]);
-        var drivePrefix = Gen.Zip(driveLetter, separator).Select(static t =>
+        var drivePrefix = driveLetter.Zip(separator).Select(static t =>
         {
             var (driveLetter, separator) = t;
             return driveLetter + separator;
         });
-        return Gen.Zip(drivePrefix, LinuxPathItemsGenerator()).Select(static t =>
+        return drivePrefix.Zip(LinuxPathItemsGenerator()).Select(static t =>
         {
             var (prefix, items) = t;
             items.Insert(0, prefix);
@@ -201,22 +196,27 @@ internal static class PathGenerators
 
 public class AnyOsPath
 {
+    [UsedImplicitly]
     public static Arbitrary<List<string>> Paths()
     {
-        return Arb.From(Gen.Or(PathGenerators.LinuxPathItemsGenerator(), PathGenerators.WindowsPathItemsGenerator()));
+        return Arb.From(PathGenerators.LinuxPathItemsGenerator().Or(PathGenerators.WindowsPathItemsGenerator()));
     }
 }
 
+[UsedImplicitly]
 public class LinuxPath
 {
+    [UsedImplicitly]
     public static Arbitrary<List<string>> Paths()
     {
         return Arb.From(PathGenerators.LinuxPathItemsGenerator());
     }
 }
 
+[UsedImplicitly]
 public class WindowsPath
 {
+    [UsedImplicitly]
     public static Arbitrary<List<string>> Paths()
     {
         return Arb.From(PathGenerators.WindowsPathItemsGenerator());
