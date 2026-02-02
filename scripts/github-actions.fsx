1b709ac4f042b15b7de3154805f911b681932f01
Friedrich von Never <friedrich@fornever.me>
2026-02-02 23:21:07 +0100

    CI: improve the Generaptor workflow

--- scripts/github-actions.fsx
+++ scripts/github-actions.fsx
@@ -5,14 +5,12 @@ let licenseHeader = """
 
 # This file is auto-generated.""".Trim()
 
-#r "nuget: Generaptor.Library, 1.9.1"
+#r "nuget: Generaptor, 1.9.1"
 
 open System
 open Generaptor
 open Generaptor.GitHubActions
 open type Generaptor.GitHubActions.Commands
-open type Generaptor.Library.Actions
-open type Generaptor.Library.Patterns
 
 let mainBranch = "main"
 let ubuntu = "ubuntu-24.04"
@@ -36,43 +34,83 @@ let workflows = [
         yield! actions
     ]
 
-    workflow "main" [
-        name "Main"
-        yield! mainTriggers
+    let checkOut = step(
+        name = "Check out the sources",
+        usesSpec = Auto "actions/checkout"
+    )
 
-        job "verify-workflows" [
-            runsOn "ubuntu-24.04"
+    let dotNetJob id steps =
+        job id [
+            setEnv "DOTNET_CLI_TELEMETRY_OPTOUT" "1"
+            setEnv "DOTNET_NOLOGO" "1"
+            setEnv "NUGET_PACKAGES" "${{ github.workspace }}/.github/nuget-packages"
 
-            checkout
+            checkOut
             step(
                 name = "Set up .NET SDK",
                 usesSpec = Auto "actions/setup-dotnet"
             )
+            step(
+                name = "Cache NuGet packages",
+                usesSpec = Auto "actions/cache",
+                options = Map.ofList [
+                    "key", "${{ runner.os }}.nuget.${{ hashFiles('**/*.*proj', '**/*.props') }}"
+                    "path", "${{ env.NUGET_PACKAGES }}"
+                ]
+            )
+
+            yield! steps
+        ]
+
+    workflow "main" [
+        name "Main"
+        yield! mainTriggers
+
+        dotNetJob "verify-workflows" [
+            runsOn "ubuntu-24.04"
             step(run = "dotnet fsi ./scripts/github-actions.fsx verify")
         ]
 
-        job "check" [
-            checkout
-            yield! dotNetBuildAndTest(projectFileExtensions = [".csproj"])
-        ] |> addMatrix images
+        dotNetJob "check" [
+            strategy(failFast = false, matrix = [
+                "image", [
+                    "macos-14"
+                    "ubuntu-24.04"
+                    "ubuntu-24.04-arm"
+                    "windows-11-arm"
+                    "windows-2025"
+                ]
+            ])
+            runsOn "${{ matrix.image }}"
+
+            step(
+                name = "Build",
+                run = "dotnet build"
+            )
+            step(
+                name = "Test",
+                run = "dotnet test",
+                timeoutMin = 10
+            )
+        ]
 
         job "licenses" [
             runsOn ubuntu
-            checkout
+            checkOut
 
             step(name = "REUSE license check", usesSpec = Auto "fsfe/reuse-action")
         ]
 
         job "encoding" [
             runsOn ubuntu
-            checkout
+            checkOut
 
             step(name = "Verify encoding", shell = "pwsh", run = "scripts/Test-Encoding.ps1")
         ]
 
-        job "nowarn-empty" [
+        dotNetJob "nowarn-empty" [
             runsOn ubuntu
-            checkout
+            checkOut
 
             step(name = "Verify with NoWarn as empty", run = "dotnet build /p:NoWarn='' --no-incremental")
         ]
@@ -84,15 +122,22 @@ let workflows = [
         onPushTags "v*"
         job "nuget" [
             runsOn ubuntu
-            checkout
+            checkOut
             jobPermission(PermissionKind.Contents, AccessKind.Write)
 
             let configuration = "Release"
 
             let versionStepId = "version"
             let versionField = "${{ steps." + versionStepId + ".outputs.version }}"
-            getVersionWithScript(stepId = versionStepId, scriptPath = "scripts/Get-Version.ps1")
-            dotNetPack(version = versionField)
+            step(
+                id = "version",
+                name = "Get version",
+                shell = "pwsh",
+                run = "echo \"version=$(scripts/Get-Version.ps1 -RefName $env:GITHUB_REF)\" >> $env:GITHUB_OUTPUT"
+            )
+            step(
+                run = "dotnet pack --configuration Release -p:Version=${{ steps.version.outputs.version }}"
+            )
 
             let releaseNotes = "./release-notes.md"
             step(
@@ -112,23 +157,35 @@ let workflows = [
                 yield! artifacts "TruePath" true
                 yield! artifacts "TruePath.SystemIo" true
             ]
-            uploadArtifacts [
-                releaseNotes
-                yield! allArtifacts
-            ]
-            yield! ifCalledOnTagPush [
-                createRelease(
-                    name = $"TruePath v{versionField}",
-                    releaseNotesPath = releaseNotes,
-                    files = allArtifacts
-                )
-                yield! pushToNuGetOrg "NUGET_TOKEN_TRUE_PATH" (
-                    artifacts "TruePath" false
-                )
-                yield! pushToNuGetOrg "NUGET_TOKEN_TRUE_PATH_SYSTEM_IO" (
-                    artifacts "TruePath.SystemIo" false
-                )
-            ]
+
+            step(
+                name = "Upload artifacts",
+                usesSpec = Auto "actions/upload-artifact",
+                options = Map.ofList [
+                    "path", [ releaseNotes; yield! allArtifacts ] |> String.concat "\n"
+                ]
+            )
+
+            step(
+                condition = "startsWith(github.ref, 'refs/tags/v')",
+                name = "Create a release",
+                usesSpec = Auto "softprops/action-gh-release",
+                options = Map.ofList [
+                    "body_path", "./release-notes.md"
+                    "files", allArtifacts |> String.concat "\n"
+                    "name", $"TruePath v{versionField}"
+                ]
+            )
+            step(
+                condition = "startsWith(github.ref, 'refs/tags/v')",
+                name = "Push TruePath to NuGet",
+                run = $"dotnet nuget push ./TruePath/bin/Release/TruePath.{versionField}.nupkg --source https://api.nuget.org/v3/index.json --api-key ${{{{ secrets.NUGET_TOKEN_TRUE_PATH }}}}"
+            )
+            step(
+                condition = "startsWith(github.ref, 'refs/tags/v')",
+                name = "Push TruePath.SystemIo to NuGet",
+                run = $"dotnet nuget push ./TruePath.SystemIo/bin/Release/TruePath.SystemIo.{versionField}.nupkg --source https://api.nuget.org/v3/index.json --api-key ${{{{ secrets.NUGET_TOKEN_TRUE_PATH_SYSTEM_IO }}}}"
+            )
         ]
     ]
 
@@ -146,10 +203,7 @@ let workflows = [
         job "publish-docs" [
             environment(name = "github-pages", url = "${{ steps.deployment.outputs.page_url }}")
             runsOn "ubuntu-24.04"
-            step(
-                name = "Checkout",
-                usesSpec = Auto "actions/checkout"
-            )
+            checkOut
             step(
                 name = "Set up .NET SDK",
                 usesSpec = Auto "actions/setup-dotnet",
