c1dafb7b6e1bdf12252e686fed17fbfde12a774a
Friedrich von Never <friedrich@fornever.me>
2026-02-05 23:46:36 +0100

    CI: update to macos-15

--- scripts/github-actions.fsx
+++ scripts/github-actions.fsx
@@ -74,7 +74,7 @@ let workflows = [
         dotNetJob "check" [
             strategy(failFast = false, matrix = [
                 "image", [
-                    "macos-14"
+                    "macos-15"
                     "ubuntu-24.04"
                     "ubuntu-24.04-arm"
                     "windows-11-arm"
