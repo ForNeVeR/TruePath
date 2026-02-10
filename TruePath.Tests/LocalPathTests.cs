e4dc94a046578b5823e1c7fb9a0a2d96cd22b4bd
pkazakov-dev <pkazakov.dev@gmail.com>
2026-02-10 12:04:27 +0500

    Remove not used ns usings

--- TruePath.Tests/LocalPathTests.cs
+++ TruePath.Tests/LocalPathTests.cs
@@ -1,8 +1,6 @@
-// SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.me>
+// SPDX-FileCopyrightText: 2024-2026 TruePath contributors <friedrich@fornever.me>
 //
 // SPDX-License-Identifier: MIT
-
-using Xunit.Abstractions;
 
 namespace TruePath.Tests;
 
